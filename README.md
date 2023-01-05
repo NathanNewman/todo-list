fde36c2151da6dc397c57d6c67408f747243a44e
Nathan Newman <lordnate.moose@gmail.com>
2023-01-05 10:09:08 -0600

    added autocomplete

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Deploy: https://nathannewman.github.io/todo-list/
 
-This is my first JavaScript project. It's a very simple todo list.
+This is my first JavaScript project. It's a very simple todo list. Users input a task into the input field and then click Add Task. The task in then added to an unordered list below. Each task also has a delete button so tasks can be removed. The todo list is saved to local storage, so that it loads when the user revisits the page.
+
+Additional functionality was add for an autocomplete. The autocomplete terms are limited to: "Dinner", "Laundry", "Lunch", "Mow", "Homework", "Study", "Breakfast", and "Clean".
--- script.js
+++ script.js
@@ -1,60 +1,83 @@
 // Global variables
-const form = document.querySelector('#inputTask');
-const input = document.querySelector('#toDo');
-const toDoList = document.querySelector('#list');
+const form = document.querySelector("#inputTask");
+const input = document.querySelector("#toDo");
+const toDoList = document.querySelector("#list");
 
 // load from localStorage
-const savedTasks = JSON.parse(localStorage.getItem('toDos')) || [];
-for (let i = 0; i < savedTasks.length; i++){
-    const newTask = document.createElement('li');
-    const removeBtn = document.createElement('button');
-    removeBtn.innerText = 'X';
-    newTask.innerText = savedTasks[i].task.slice(0, -1);
-    newTask.appendChild(removeBtn);
-    newTask.isCompleted = savedTasks[i].isCompleted ? true : false;
-    toDoList.appendChild(newTask);
-    if (newTask.isCompleted){
-        newTask.style.textDecoration = 'line-through';
-    }
+const savedTasks = JSON.parse(localStorage.getItem("toDos")) || [];
+for (let i = 0; i < savedTasks.length; i++) {
+  const newTask = document.createElement("li");
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = "X";
+  newTask.innerText = savedTasks[i].task.slice(0, -1);
+  newTask.appendChild(removeBtn);
+  newTask.isCompleted = savedTasks[i].isCompleted ? true : false;
+  toDoList.appendChild(newTask);
+  if (newTask.isCompleted) {
+    newTask.style.textDecoration = "line-through";
+  }
 }
 
 // add task
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const newTask = document.createElement('li');
-    const removeBtn = document.createElement('button');
-    removeBtn.innerText = 'X';
-    newTask.innerText = input.value;
-    newTask.appendChild(removeBtn);
-    newTask.isComplete = false;
-    input.value = '';
-    toDoList.appendChild(newTask);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newTask = document.createElement("li");
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = "X";
+  newTask.innerText = input.value;
+  newTask.appendChild(removeBtn);
+  newTask.isComplete = false;
+  input.value = "";
+  toDoList.appendChild(newTask);
 
-    // save to localStorage
-    savedTasks.push({task: newTask.innerText, isComplete: false});
-    localStorage.setItem('toDos', JSON.stringify(savedTasks));
+  // save to localStorage
+  savedTasks.push({ task: newTask.innerText, isComplete: false });
+  localStorage.setItem("toDos", JSON.stringify(savedTasks));
 });
 
 // cross off and/or remove items from list
-toDoList.addEventListener('click', function(e){
-    let clickedListItem = e.target
+toDoList.addEventListener("click", function (e) {
+  let clickedListItem = e.target;
 
-    if (!clickedListItem.isComplete){
-        clickedListItem.style.textDecoration = 'line-through';
-        clickedListItem.isComplete = true;
-    } else {
-        clickedListItem.style.textDecoration = 'none';
-        clickedListItem.isComplete = false;
-    }
-    if(e.target.tagName === 'BUTTON'){
-        e.target.parentElement.remove();
-    }
+  if (!clickedListItem.isComplete) {
+    clickedListItem.style.textDecoration = "line-through";
+    clickedListItem.isComplete = true;
+  } else {
+    clickedListItem.style.textDecoration = "none";
+    clickedListItem.isComplete = false;
+  }
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+  }
 
-    // save to localStorage
-    for (let i = 0; i < savedTasks.length; i++){
-        if (savedTasks[i].task === clickedListItem.innerText){
-            savedTasks[i].isCompleted = clickedListItem.isCompleted;
-            localStorage.setItem('toDos', JSON.stringify(savedTasks));
-        }
+  // save to localStorage
+  for (let i = 0; i < savedTasks.length; i++) {
+    if (savedTasks[i].task === clickedListItem.innerText) {
+      savedTasks[i].isCompleted = clickedListItem.isCompleted;
+      localStorage.setItem("toDos", JSON.stringify(savedTasks));
     }
+  }
+});
+
+// adds autocomplete function to input text
+
+const taskArray = ["Dinner", "Laundry", "Lunch", "Mow", "Homework", "Study", "Breakfast", "Clean"];
+
+input.addEventListener("input", function (e) {
+  const input = e.target;
+  const text = input.value;
+
+  if (text.length > 0) {
+    let newText = "";
+    for (let task of taskArray) {
+      if (task.startsWith(text)) {
+        newText = task.slice(text.length, task.length);
+      }
+    }
+    if (newText) {
+      completeText = text + newText;
+      input.value = completeText;
+      input.setSelectionRange(text.length, completeText.length);
+    }
+  }
 });
